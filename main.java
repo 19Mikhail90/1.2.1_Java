450a7d555298cc571a67a541e44e9576706df1bc
Mikhail Gozhenko <sacred9900@gmail.com>
2021-08-12 00:02:48 +0900

    Add issues

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int currentBalance;
+        currentBalance = 2000000000 ;
+        int transferAmount;
+        transfer_amount = 500000000 ;
+        int finalBalance = currentBalance + transferAmount;
+        System.out.println(finalBalance);
+    }
+}
